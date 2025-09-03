94ffe980f729eb7aa00b9c083f0fb0789c9a3195
TambyGit <hei.tamby@gmail.com>
2025-09-03 09:56:54 +0300

    feat: All Code is here

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Characteristics(BaseModel):
+    ram_memory: float
+    rom_memory: float
+
+
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristics
+
+phones = []
+
 @app.get("/health")
-async def root():
-    return {"message:" "Ok"}
+async def health():
+    return "OK"
 
-@app.post("/phone")
-async def create_phone(phone: dict):
-    return {"message": "Phone created", "phone": phone}
+@app.post("/phones", status_code=201)
+async def create_phone(phone: Phone):
+    phones.append(phone.dict())
+    return {"message": "Phone created"}
 
-@app.get("/phone")
+@app.get("/phones")
 async def get_phones():
-    return {"message": "List of phones", "phones": []}
+    return phones
 
-@app.get("/phone/{phone_id}")
-async def get_phone(phone_id: int):
-    return {"message": "Phone details", "phone": {"id": phone_id, "brand": "Example", "model": "Example"}}
-
-@app.put("/phone/{phone_id}")
-async def update_phone(phone_id: int, phone: dict):
-    return {"message": "Phone updated", "phone": {"id": phone_id, **phone}}
+@app.get("/phones/{id}")
+async def get_phone(id: str):
+    for phone in phones:
+        if phone["identifier"] == id:
+            return phone
+    raise HTTPException(status_code=404, detail="Phone with provided id does not exist or was not found")
